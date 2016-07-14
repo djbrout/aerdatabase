c293cb0c939facf762b2504828f71c844653709c
Dillon Brout <djbrout@gmail.com>
2016-07-14 14:26:15 -0400

    returns html

--- insertest.py
+++ insertest.py
@@ -1,6 +1,6 @@
 import connection as c
 db,de = c.con()
-
+from IPython.core.display import HTML
 
 def go(name = 'John Doe', age=46, country='China',gender=None):
 
@@ -9,6 +9,6 @@ def go(name = 'John Doe', age=46, country='China',gender=None):
     # Insert a new record.
     table.insert(dict(name=name, age=age, country=country,gender=gender))
 
-    print('Inserted into database succesfully...')
+    return HTML('<h4>Inserted into database succesfully...</h4>')
 
     #df.to_sql("my_table_name", engine)
