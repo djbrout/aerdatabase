4be8d2166e85ca78d0835650c595c7b42769558a
Dillon Brout <djbrout@gmail.com>
2016-07-14 01:03:02 -0400

    test

--- imports.py
+++ imports.py
@@ -7,14 +7,16 @@ def get():
     from IPython.display import display
     import pandas as pd
     import datetime as dt
+    import warnings
+    warnings.filterwarnings("ignore")
     import matplotlib.pyplot as plt
     import matplotlib
     matplotlib.style.use('ggplot')
     matplotlib.rcParams['figure.figsize'] = (8.0, 6.0)
     import numpy as np
     from IPython.core.display import HTML
-    import urllib2
-    HTML(urllib2.urlopen('http://bit.ly/1Bf5Hft').read())
+    import urllib.request
+    HTML(urllib.request.urlopen('http://bit.ly/1Bf5Hft').read())
     import insertest as insert
 
     return c, db, de, display, pd, dt, plt, np, insert
